4dcfaca45fcc0c4a19404a87395f28ae745b134b
Quang Duong Nguyen <quanguyendg@gmail.com>
2022-10-15 17:46:52 -0500

    add comment cell

---
+++ Parstagram/Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Nguyễn Đương Quang on 10/15/22.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
