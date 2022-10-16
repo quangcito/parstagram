f97501cc1c72fdba30b4b9496df88ff110b2f518
Quang Duong Nguyen <quanguyendg@gmail.com>
2022-10-15 22:44:38 -0500

    add comment bar

--- Parstagram/Parstagram/CommentCell.swift
+++ Parstagram/Parstagram/CommentCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CommentCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
